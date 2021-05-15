8f0c038338f70f505cd0988fa1f622a975a24ae4
aquma25 <h.aquma25@gmail.com>
2021-05-15 19:15:38 +0900

    HTTP通信時の例外処理の追加

--- net_http_ruby.rb
+++ net_http_ruby.rb
@@ -2,5 +2,9 @@ require 'net/http'
 require 'uri'
 
 url = "https://www.tagindex.com/html_tag/link/image/neko_s.jpg"
-puts Net::HTTP.get_response(URI.parse(url)).code
+begin
+  puts Net::HTTP.get_response(URI.parse(url)).code == "200" ? "true!!" : "false!!"
+rescue
+  puts "error_false!!"
+end
 
